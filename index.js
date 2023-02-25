f357b7ca8ae38cc78f42bd7dff8ccfd832908a7a
rahil_h_khane <khanrahil786086@gmail.com>
2023-02-25 08:17:36 +0530

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = new express()
 const path = require('path')
-
+const ejs = require('ejs') 
+app.set('view engine','ejs')
 app.use(express.static('public'))
 app.listen(4000, ()=>{
 console.log('App listening on port 4000')
 })
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index')
 })
 app.get("/about", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about')
 })
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact')
 })
 app.get("/post", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post')
 })
